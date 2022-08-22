e8e1f753c2028f05a8fdcfd0c50d936c5c2d1250
ps13g <saketh.2004@gmail.com>
2022-08-22 12:13:05 -0600

    Inputs 1

--- src/Inputs.java
+++ src/Inputs.java
@@ -3,11 +3,8 @@ public class Inputs {
     public static void main(String[] args) {
         Scanner input;
         input = new Scanner(System.in);
-
         System.out.println("Enter name: ");
-
         String name = input.nextLine();
-
         System.out.println("Hello " + name );
     }
 }
