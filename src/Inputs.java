265b766fef0a8f9f023c5c370ccac86e99065acc
ps13g <saketh.2004@gmail.com>
2022-08-22 12:07:46 -0600

    Inputs 1

--- src/Inputs.java
+++ src/Inputs.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Inputs {
+import java.util.Scanner;
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input;
+        input = new Scanner(System.in);
+
+        System.out.println("Enter name: ");
+
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name );
+    }
 }
